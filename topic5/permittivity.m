f93ef55a72f6690ba14eecf96ff68056465500ca
Mine4ever <935952896@qq.com>
2018-07-30 11:45:01 +0800

    ll

--- topic5/permittivity.m
+++ topic5/permittivity.m
@@ -3,7 +3,7 @@
 
 clear all;
 
-[fre,s11_m,s11_p,s21_m,s21_p,s12_m,s12_p,s22_m,s22_p]=textread('PART.s2p','%f%f%f%f%f%f%f%f%f','headerlines',8);
+[fre,s11_m,s11_p,s21_m,s21_p,s12_m,s12_p,s22_m,s22_p]=textread('1.s2p','%f%f%f%f%f%f%f%f%f','headerlines',8);
 
 % for i=1:800
 % if s11_p(i)<0
@@ -21,47 +21,50 @@ clear all;
 % end
 
 
-TEaLs11p=10.^(s11_m/20);
-TEaLs21p=10.^(s21_m/20);
+TEaLs11p=10.^(s11_m/20);    % 将分贝转为数值
+TEaLs21p=10.^(s21_m/20);    % 将分贝转为数值
 
 
 TEas11a=s11_p;
 TEas21a=s21_p;
 
 
-TEaRs11a=TEas11a*pi/180;
-TEaRs21a=TEas21a*pi/180;
+TEaRs11a=TEas11a*pi/180;    % 将角度转为弧度
+TEaRs21a=TEas21a*pi/180;    % 将角度转为弧度
 
 % figure;
 % plot(fre,TEaRs11a,'r',fre,TEaRs21a,'b');
 % legend('Rs11-arg:rad','Rs21-arg:rad');
 % grid
 
-TEas11=TEaLs11p.*(cos(TEaRs11a)-1i*sin(TEaRs11a));
-TEas21=TEaLs21p.*(cos(TEaRs21a)-1i*sin(TEaRs21a));
+TEas11=TEaLs11p.*(cos(TEaRs11a)-1i*sin(TEaRs11a));  % 拼合相、频，组成时域表达式
+TEas21=TEaLs21p.*(cos(TEaRs21a)-1i*sin(TEaRs21a));  % 拼合相、频，组成时域表达式
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 Cv=3*10^8;
-wa=0.02286;%WR-90
-kx=pi/wa;
+% wa = 0.0225; % WR-90
+wa = 0.047;
+kx=pi/wa;   % kc = sqrt((m*pi/a)^2 + (n*pi/b)^2)
 k0=2*pi*fre/Cv;
-ka=sqrt(k0.^2-kx^2);
-da=0.00668;%thick of the slab under test  被测量样品厚度
+ka=sqrt(k0.^2-kx^2);    % beta相位常数 = sqrt(k^2 - kc^2)
+% da = 0.0097;%thick of the slab under test  被测量样品厚度
+da = 0.0156;
 d1a=0; %length between slab with detector of S11 靠近port1的距离
-t14=0.00968;%1/4 wavelength waveguide thickness   
+% t14 = 0.0097;%1/4 wavelength waveguide thickness   
+t14 = 0.0196;
 dta=t14-da; %length between slab with detector of S21   
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-ra=TEas11.*exp(-2i*d1a*ka);
+ra=TEas11.*exp(-2i*d1a*ka);                         % 反射系数加指数项
 
-ta=TEas21.*exp(-1i*(d1a+dta)*ka);
+ta=TEas21.*exp(-1i*(d1a+dta)*ka);                   % 透射系数加指数项
 
-z1a=sqrt(((1+ra).^2-ta.^2)./((1-ra).^2-ta.^2));
-z2a=-sqrt(((1+ra).^2-ta.^2)./((1-ra).^2-ta.^2));
+z1a=sqrt(((1+ra).^2-ta.^2)./((1-ra).^2-ta.^2));     % ？？？
+z2a=-sqrt(((1+ra).^2-ta.^2)./((1-ra).^2-ta.^2));    % ？？？
 
 delta=0.01;
 
-ephi1a=abs(ta./(1-ra.*(z1a-1)./(z1a+1)))<=1;
-ephi2a=abs(ta./(1-ra.*(z2a-1)./(z2a+1)))<=1;
+ephi1a=abs(ta./(1-ra.*(z1a-1)./(z1a+1)))<=1;        % logical number，非0即1
+ephi2a=abs(ta./(1-ra.*(z2a-1)./(z2a+1)))<=1;        % logical number，非0即1
 
 zma=(abs(real(z1a))>=delta).*(z1a.*sign(real(z1a)))+(abs(real(z1a))<delta).*(ephi1a.*z1a+ephi2a.*z2a);
 
@@ -91,7 +94,7 @@ na3n=-dna*3+nma;
 na4p=dna*4+nma;
 na4n=-dna*4+nma;
 %nma 是没有额外波长的长度（样品比较小），加减dna是有额外的波长（样品比较大），一个一个试看哪个对
-nnnna=nma;%choose phase nma, na1p, na1n
+nnnna = na1p;%choose phase nma, na1p, na1n
 % nnnna(1:98)=na1p(1:98);%for castor oil
 % nnnna(99:end)=na1p(99:end);%for mineral oil
 % nnnna=zeros(size(nma));
@@ -172,16 +175,17 @@ dnb=2*pi./(kb*db);
 % axis([1.6 2.4 -5 5]);
 
 nb1p=dnb+nmb;
-nb1n=-dnb+nmb;
-nb2p=dnb*2+nmb;
-nb2n=-dnb*2+nmb;
-nb3p=dnb*3+nmb;
-nb3n=-dnb*3+nmb;
-nb4p=dnb*4+nmb;
-nb4n=-dnb*4+nmb;
-%nnnnb考虑了各向异性，这里没用
+% nb1n=-dnb+nmb;
+% nb2p=dnb*2+nmb;
+% nb2n=-dnb*2+nmb;
+% nb3p=dnb*3+nmb;
+% nb3n=-dnb*3+nmb;
+% nb4p=dnb*4+nmb;
+% nb4n=-dnb*4+nmb;
+% nnnnb考虑了各向异性，这里没用
 
-nnnnb=nmb;%choose phase
+nnnnb = nmb;    %choose phase
+
 % nnnnb(58:end)=nb1p(58:end);%for castor oil
 % nnnnb(99:end)=nb1p(99:end);%for mineral oil
 % nnnnb=zeros(size(nmb));
@@ -198,7 +202,7 @@ uuuub=nnnnb.*zmb;
 eeeea=(nnnna.^2.*ka.^2+uuuua./uuuub.*kx^2)./k0.^2./uuuua;
 eeeeb=(nnnnb.^2.*kb.^2+uuuub./uuuua.*kx^2)./k0.^2./uuuub;
 
-if isempty(find(abs(diff(real(uuuua)))>=0.1, 1))==0%discontinuous
+if isempty(find(abs(diff(real(uuuua)))>=0.1, 1))==0 %discontinuous
     D=find(abs(diff(real(uuuua)))>=0.1);
     uuuua(1:D)=(nnnna(1:D)-dnb(1:D)).*zma(1:D);
     uuuub(1:D)=(nnnnb(1:D)-dnb(1:D)).*zmb(1:D);
@@ -214,7 +218,7 @@ title('Measured S-Parameter Magnitudes','Fontsize',14,'Fontname','Helvetica');
 xlabel('Frequency (GHz)','Fontsize',14,'Fontname','Helvetica');
 ylabel('dB','Fontsize',14,'Fontname','Helvetica');
 set(gca,'Fontsize',14,'Fontname','Helvetica');
-xlim([8 12]);
+% xlim([8 12]);
 
 figure
 plot(fre/1e9,s11_p,'r',fre/1e9,s21_p,'b',fre/1e9,s12_p,'k',fre/1e9,s22_p,'g','Linewidth',1.7);grid
@@ -224,7 +228,7 @@ title('Measured S-Parameter Phases','Fontsize',14,'Fontname','Helvetica');
 xlabel('Frequency (GHz)','Fontsize',14,'Fontname','Helvetica');
 ylabel('degree','Fontsize',14,'Fontname','Helvetica');
 set(gca,'Fontsize',14,'Fontname','Helvetica');
-xlim([8 12]);
+% xlim([8 12]);
 
 figure
 plot(fre/1e9,real(eeeea),'r',fre/1e9,imag(eeeea),'b','Linewidth',1.7);grid
@@ -233,23 +237,25 @@ legend('Re\{\epsilon\}','Im\{\epsilon\}');
 title('Extracted Permittivity','Fontsize',14,'Fontname','Helvetica');
 xlabel('Frequency (GHz)','Fontsize',14,'Fontname','Helvetica');
 set(gca,'Fontsize',14,'Fontname','Helvetica');
-xlim([8 12]);
+% xlim([8 12]);
 
-figure
-plot(fre/1e9,real(uuuua),'r',fre/1e9,imag(uuuua),'b','Linewidth',1.7);grid
-hold on
-legend('Re\{\mu\}','Im\{\mu\}');
-title('Extracted Permeability','Fontsize',14,'Fontname','Helvetica');
-xlabel('Frequency (GHz)','Fontsize',14,'Fontname','Helvetica');
-set(gca,'Fontsize',14,'Fontname','Helvetica');
-xlim([8 12]);
-
-figure
-plot(fre/1e9,imag(eeeea)./real(eeeea),'Linewidth',1.7);grid
-title('Calculated Dielectric Loss Tangent','Fontsize',14,'Fontname','Helvetica');
-xlabel('Frequency (GHz)','Fontsize',14,'Fontname','Helvetica');
-set(gca,'Fontsize',14,'Fontname','Helvetica');
-xlim([8 12]);
+% figure
+% plot(fre/1e9,real(uuuua),'r',fre/1e9,imag(uuuua),'b','Linewidth',1.7);grid
+% hold on
+% legend('Re\{\mu\}','Im\{\mu\}');
+% title('Extracted Permeability','Fontsize',14,'Fontname','Helvetica');
+% xlabel('Frequency (GHz)','Fontsize',14,'Fontname','Helvetica');
+% set(gca,'Fontsize',14,'Fontname','Helvetica');
+% % xlim([8 12]);
+% saveas(gcf,'CastorOil_Type4_Sp_U_New.fig')
+% 
+% figure
+% plot(fre/1e9,imag(eeeea)./real(eeeea),'Linewidth',1.7);grid
+% title('Calculated Dielectric Loss Tangent','Fontsize',14,'Fontname','Helvetica');
+% xlabel('Frequency (GHz)','Fontsize',14,'Fontname','Helvetica');
+% set(gca,'Fontsize',14,'Fontname','Helvetica');
+% % xlim([8 12]);
+% saveas(gcf,'CastorOil_Type4_Sp_Loss_New.fig')
 
 
 %Fit the extract ep and mu into a line using Least Squares Polynomial Fit
